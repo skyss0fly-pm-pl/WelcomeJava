60e2db7d54a2ad8691c0c1671a9088926712cd4d
skyss0fly-pm-pl <144737802+skyss0fly-pm-pl@users.noreply.github.com>
2024-07-09 16:31:25 +1000

    Update JoinListener.java

--- src/skyss0fly/JoinListener.java
+++ src/skyss0fly/JoinListener.java
@@ -2,7 +2,7 @@ import org.bukkit.event.player.PlayerJoinEvent
 public class JoinListener implements Listener {
 
   @EventHandler(priority = EventPriority.HIGH)
-public static void JoinListener(String[] args) {
+public static void JoinListener(String[] args, PlayerJoinEvent(@NotNull Player playerJoined, @Nullable String joinMessage)) {
   getServer().getPluginManager().registerEvents(new JoinListener(), this);
 String Message = new Message();
   message.Message();
