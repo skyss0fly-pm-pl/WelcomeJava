be7c57ec84aa6937f82af3515eabd614ddc8993a
skyss0fly-pm-pl <144737802+skyss0fly-pm-pl@users.noreply.github.com>
2024-07-09 16:26:15 +1000

    Update and rename Main.java to JoinListener.java

---
+++ src/skyss0fly/JoinListener.java
@@ -0,0 +1,10 @@
+public class JoinListener implements Listener {
+
+  @EventHandler
+public static void JoinListener(String[] args) {
+  getServer().getPluginManager().registerEvents(new JoinListener(), this);
+String Message = new Message();
+  message.Message();
+
+
+}
--- src/skyss0fly/Main.java
+++
@@ -1,9 +0,0 @@
-public class Maim implements Listener {
-
-  @EventHandler
-public static void main(String[] args) {
-String Message = new Message();
-  message.Message();
-
-
-}
